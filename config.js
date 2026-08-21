ff663fc14349d2d95360ce12254327db7cbffbf1
marketgoverifikasi-oss <marketgoverifikasi@gmail.com>
2026-08-21 08:39:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    botToken: process.env.BOT_TOKEN || "ISI_TOKEN_BOT_DARI_BOTFATHER_DI_SINI",
+    botName: "Alight Motion Creator",
+    ownerName: "Barikz",
+    ownerId: process.env.OWNER_ID || "ISI_ID_TELEGRAM_KAMU_DI_SINI", 
+    askey: "AK-yrDsjDEHSj",
+    pakasirProject: process.env.PAKASIR_PROJECT || "ISI_SLUG_PROJECT_PAKASIR", 
+    pakasirApiKey: process.env.PAKASIR_API_KEY || "ISI_API_KEY_PAKASIR",       
+    channelUsername: "@LyraGroupEntertainment", 
+    channelLink: "https://t.me/LyraGroupEntertainment",
+    startImage: "https://fybot.my.id/7npfrpq.jpg",
+    prefix: "/",
+    version: "11.0.0 (Vercel Serverless Dashboard)"
+};
